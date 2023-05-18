a759becea40ae210ed09e299592e3272053a7372
Krisna31 <81416312+krisna31@users.noreply.github.com>
2023-05-18 16:21:35 +0700

    write gate define but i think still wrong

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,10 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+
+use App\Models\Role;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -21,6 +24,8 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::define('isSuperAdmin', fn($user) => $user->role_id === Role::$IS_SUPERADMIN);
+        Gate::define('isAdmin', fn($user) => $user->role_id === Role::$IS_ADMIN);
+        Gate::define('isUser', fn($user) => $user->role_id === Role::$IS_USER);
     }
 }
