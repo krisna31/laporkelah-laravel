880cd796faafdc47bfadfd430d85057602c5d8ed
Krisna31 <81416312+krisna31@users.noreply.github.com>
2023-05-30 09:57:43 +0700

    done for admin

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -72,7 +72,9 @@ class DashboardController extends Controller
                 $query->where('id', auth()->user()->company_id)
                     ->orWhere('is_public', 1);
             })->get();
-            $reports = Report::orderBy('created_at', 'desc')->get();
+            $reports = Report::where(function ($query) {
+                $query->where('company_id', auth()->user()->company_id);
+            })->orderBy('created_at', 'desc')->get();
             $companyId = auth()->user()->company_id;
             $chart = new LaravelChart([
                 'chart_title' => 'Users by years',
@@ -91,6 +93,7 @@ class DashboardController extends Controller
                 'group_by_period' => 'year',
                 'chart_type' => 'bar',
                 'chart_color' => '255,0,0',
+                'where_raw' => "company_id = $companyId",
             ], [
                 'chart_title' => 'Comments by years',
                 'report_type' => 'group_by_date',
@@ -99,6 +102,7 @@ class DashboardController extends Controller
                 'group_by_period' => 'year',
                 'chart_type' => 'bar',
                 'chart_color' => '0,255,0',
+                'where_raw' => "report_id in(select id from reports where company_id = $companyId)",
             ]);
 
             $chart2 = new LaravelChart([
@@ -108,7 +112,8 @@ class DashboardController extends Controller
                 'group_by_field' => 'created_at',
                 'group_by_period' => 'year',
                 'chart_type' => 'pie',
-                'style_class' => 'w-1/2 h-1/2'
+                'style_class' => 'w-1/2 h-1/2',
+                'where_raw' => "company_id = $companyId",
             ]);
 
             $chart3 = new LaravelChart([
@@ -119,6 +124,7 @@ class DashboardController extends Controller
                 'group_by_period' => 'year',
                 'chart_type' => 'pie',
                 'chart_color' => '10, 20, 30',
+                'where_raw' => "company_id = $companyId",
             ]);
         }
 
